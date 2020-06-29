bf8cf2775c61e1d8756f81a4b924d6878ce9cda0
odpr <67262187+odpr@users.noreply.github.com>
2020-06-29 13:15:00 +0200

    Push 4

--- EX_03/t-shirt.php
+++ EX_03/t-shirt.php
@@ -5,7 +5,7 @@ $couleur="blanc";
 $prix="10.50";
 $quantité="10";
 $nombre="3";
-$montant3=$prix*$nombre;
+$montant3=$prix*$nombre;;
 $montant10=$quantité*$prix;
 $reste=$quantité-$nombre;
 echo "<h3>Le nom du produit est $nom.</h3>";
@@ -16,3 +16,16 @@ echo "<h4>Acheter la totalité des produits disponibles coûterait $montant10.</
 echo "<h4>Si 3 produits sont vendus, il en reste $reste.</h4>";
 ?>
 
+<?php
+$disponible = true;
+
+if ($disponible == true)
+{
+    echo "<p>Le produit $nom est disponible en ligne.</p>";
+}
+
+elseif ($disponible == false)
+{
+    echo "<p>Le produit $nom n’est malheureusement plus disponible.</p>";
+}
+?>
