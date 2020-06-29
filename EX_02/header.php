3d0348062ddc09a51fd73612a4788711b2c0b0e4
odpr <67262187+odpr@users.noreply.github.com>
2020-06-29 11:41:53 +0200

    Push 1

---
+++ EX_02/header.php
@@ -0,0 +1,12 @@
+<header>
+    <title>SCC Accueil</title>
+    <nav class="menu">
+            <A HREF="vitrine-accueil.php" class="en-cours">ACCUEIL</A> 
+            <A HREF="vitrine-programme.php">PROGRAMME</A> 
+            <A HREF="vitrine-contacts.php">CONTACT</A>
+    </nav>
+
+    <div id="blocimage"> 
+        <h1>Summer Code Camp</h1>
+    </div>      
+</header>
---
+++ hello-world.php
@@ -0,0 +1,12 @@
+<html>
+    <header>
+        <title>Hello World - PHP</title>
+      </header>
+      <body>
+          <?php
+          echo "Hello World !\n";
+          echo ("Hello World !\n");
+          ?>
+
+      </body>
+</html>
